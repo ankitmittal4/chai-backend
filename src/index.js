4d08b8d0efdf90328456a52822c269f58f9a83a0
Ankit <ankitmittal0814@gmail.com>
2024-04-08 22:11:22 +0530

    Create promise

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(`Server is running on Port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Database connection Failed: ", error);
+  });
