a09d8039a0b91c4a10125d5cf9ebcf9c8f70fdc6
Ankit <ankitmittal0814@gmail.com>
2024-04-17 11:26:33 +0530

    validation in register user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,24 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
 const registerUser = asyncHandler(async (req, res) => {
   //   res.status(200).json({
   //     message: "ok",
   //   });
 
+  // Step1: get user details from frontend
   const { fullName, username, password, email } = req.body;
-
+  //handle file such as coverImage and avatar in routes
   console.log("email: ", email);
   console.log("password: ", password);
+
+  // Step2: check validation : if filed is empty
+  if (
+    [username, email, password, password].some((field) => filed?.trim === "")
+  ) {
+    throw new ApiError(400, "All fileds are required!!!");
+  }
+
+  // Step3: check if user already exists
 });
 
 export { registerUser };
