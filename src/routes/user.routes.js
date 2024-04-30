bb023b8188f6cc09b583b74c9a974e72237cc0b6
Ankit <ankitmittal0814@gmail.com>
2024-04-30 21:08:48 +0530

    Sub aggregate pipeline for user watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,8 +4,13 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
-  changeCurrentPasword,
+  changeCurrentPassword,
   getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -28,8 +33,13 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken);
-router.route("/change-password").post(changeCurrentPasword);
-router.route("/current-user").post(getCurrentUser);
+router.route("/refresh-token").post(verifyJWT, refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router.route("/update-avatar").patch(verifyJWT, updateUserAvatar);
+router.route("/update-cover-image").patch(verifyJWT, updateCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
